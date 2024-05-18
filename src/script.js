a6aa5a4a3d495506877e65c0ab8dc9139c3da50d
estacioFilho <estacioqueirozfilho@gmail.com>
2024-05-17 22:50:38 -0300

    Validando Idade maior que 18 anos

--- src/script.js
+++ src/script.js
@@ -1,13 +1,17 @@
 import campoCpf from "./valida-cpf.js";
+import maiorIdade from "./valida-idade.js";
+
 
 const camposObrigatorios = document.querySelectorAll("[required]");
-const mensagemErro = document.getElementsByClassName('mensagem-erro')[0];
 
 camposObrigatorios.forEach((campo) => {campo.addEventListener('blur', () => verificaCampo(campo))})
 
 
 function verificaCampo(campo){
     if(campo.name == 'cpf' && campo.value.length >= 11){
-        return campoCpf(campo);
+         campoCpf(campo);
+    }
+    if(campo.name = 'idade' && campo.value !=""){
+        maiorIdade(campo)
     }
 }
--- src/valida-cpf.js
+++ src/valida-cpf.js
@@ -1,9 +1,15 @@
-export default function campoCpf(campo){
+export default function campoCpf(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    console.log(numerosRepetidosCpf(cpf));
+
+    if(numerosRepetidosCpf(cpf) || verificaPrimeiroDigitoCpf(cpf) || verificaSegundoDigitoCpf(cpf)){
+        console.log('cpf inválido!')
+    }else{
+        console.log('cpf válido');
+    }
+
 }
 
-function numerosRepetidosCpf(cpf){
+function numerosRepetidosCpf(cpf) {
     const numerosRepetidos = [
         '00000000000',
         '11111111111',
@@ -16,5 +22,46 @@ function numerosRepetidosCpf(cpf){
         '88888888888',
         '99999999999'
     ]
-   return numerosRepetidos.includes(cpf);
+    return numerosRepetidos.includes(cpf);
+
 }
+    //6 1 5 4 3 6 1 8 3 1 6
+    //60 69 109 137 155 185 189 213 219 
+function verificaPrimeiroDigitoCpf(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+
+    for (let tam = 0; tam < 9; tam++){
+        soma += cpf[tam] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+
+    }
+    return soma != cpf[9];
+}
+
+
+function verificaSegundoDigitoCpf(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tam = 0; tam < 10; tam++){
+        soma += cpf[tam] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 1) {
+        soma = 0;
+
+    }
+    return soma != cpf[10];
+}
+
+
